a30e07db22e437596d0cf230f72d4d60022704ce
Peter Livesey <plivesey453@gmail.com>
2018-09-18 09:36:53 -0700

    Fixing homepage in package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 ReactDOM.render(
     <Router basename={process.env.PUBLIC_URL}>
