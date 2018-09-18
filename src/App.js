49a055fd3c88194bc4a6fa9c400fbef60b51814d
Peter Livesey <plivesey453@gmail.com>
2018-09-17 19:02:07 -0700

    Attempting to fix the router issues

--- src/App.js
+++ src/App.js
@@ -8,21 +8,18 @@ import Privacy from './Privacy.js'
 
 class App extends Component {
   render() {
-    const baseUrl = process.env.PUBLIC_URL
     return (
       <div>
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
-        <Router>
           <div className='App'>
-            <Route exact path={baseUrl + '/'} component={Home} />
+            <Route exact path={'/'} component={Home} />
 
-            <Route path={baseUrl + '/terms'} component={Terms} />
-            <Route path={baseUrl + '/terms.html'} component={Terms} />
+            <Route path={'/terms'} component={Terms} />
+            <Route path={'/terms.html'} component={Terms} />
 
-            <Route path={baseUrl + '/privacy'} component={Privacy} />
-            <Route path={baseUrl + '/privacy.html'} component={Privacy} />
+            <Route path={'/privacy'} component={Privacy} />
+            <Route path={'/privacy.html'} component={Privacy} />
           </div>
-        </Router>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Router basename={process.env.PUBLIC_URL}>
+        <App />
+    </Router>,
+    document.getElementById('root')
+)
+
+registerServiceWorker()
