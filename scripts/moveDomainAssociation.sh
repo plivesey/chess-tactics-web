03cff5e18e3c9909b94204d36aa1db6c75775640
Peter Livesey <plivesey453@gmail.com>
2019-08-07 15:27:51 +0200

    Fixing a typo

--- scripts/moveDomainAssociation.sh
+++ scripts/moveDomainAssociation.sh
@@ -4,7 +4,7 @@ git checkout gh-pages
 git pull
 mkdir .well-known
 cp apple-developer-domain-association .well-known/apple-developer-domain-association.txt
-echo 'include: [".well-known"]/npermalink: .well-known/apple-developer-domain-association.txt' > _config.yml
+echo -e 'include: [".well-known"]\npermalink: .well-known/apple-developer-domain-association.txt' > _config.yml
 git add .well-known/apple-developer-domain-association.txt
 git add _config.yml
 git commit -m 'Adding .well-known files via moveDomainAssociation.sh'
